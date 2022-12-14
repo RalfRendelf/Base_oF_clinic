bc18a09705868fb07e83376f47711f6a979332ff
RalfRendelf <vladislavungvicky@gmail.com>
2022-12-14 09:45:21 +0700

    123

--- 1232/Widget.h
+++ 1232/Widget.h
@@ -6,7 +6,7 @@ class Widget
 {
 	vector<string> menu{ "Добавить пациента","Lree","rrrrrr","rect"};
 public:
-	bool choosing(std::string Task_name);
+	bool choosing(string Task_name);
 	void Call_menu(string menu_name);
 	void Draw_menu(vector<string> &Menu, string Main_str, int &cnt);
 
